ce06a114d6f62c936ec7d62063618d47bf191278
Omar <omar.hussein@ti-media.com>
2019-06-20 01:42:13 +0100

    prettier, basic logic and button components with basic styling, basic graphql query

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import Login from './Login';
+
+import Button from './Button';
+
+// get the authentication token from local storage if it exists
+const accessToken = localStorage.getItem('token');
+
+
+fetch('https://api.github.com/graphql', {
+  method: 'POST',
+  headers: {
+    Authorization: `bearer ${accessToken}`,
+  },
+  body: JSON.stringify({
+    query: `{
+      viewer {
+        login
+      }
+    }`
+  })
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
+
+const Global = createGlobalStyle({
+  body: {
+    backgroundColor: '#fff',
+    color: '#444',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto"',
+    padding: 0,
+    margin: 0,
+  },
+  '*': {
+    boxSizing: 'border-box',
+  },
+
+  section: {
+    textAlign: 'center',
+  },
+
+  h1: {
+    background: '#4D4D4D',
+    padding: '20px',
+    margin: '0px -20px 30px -20px',
+    color: '#797979',
+    fontSize: '18px',
+    textTransform: 'uppercase',
+    fontFamily: '"Open Sans Condensed", "sans-serif"',
+    fontWeight: '100',
+
+
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Global />
+        {accessToken ? (<Button onClick={() => {
+          localStorage.clear();
+          window.location.reload();
+        }}>Logout</Button>) : (<Login />)}
+      </Fragment>
+    )
+  }
+};
 
 export default App;
---
+++ my-app/src/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+import Button from './Button';
+
+const Login = () => {
+    const [token, setToken] = useState('');
+    return (
+        <section>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    localStorage.setItem('token', token);
+                    window.location.reload();
+                }}
+            >
+                <h1>
+                    Find my repo !
+				</h1>
+                <input
+                    type="password"
+                    name="token"
+                    value={token}
+                    onChange={e => {
+                        setToken(e.target.value);
+                    }}
+                    placeholder="Github token"
+                />
+                <Button>SUBMIT</Button>
+            </form>
+        </section>
+    );
+};
+
+export default Login;
